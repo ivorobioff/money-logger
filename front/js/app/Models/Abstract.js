36bae8aaa09d63cde5a8657db51d36948d863b98
ivorobioff <i_am_vib@yahoo.com>
2013-08-02 00:32:42 +0300

    all is back

--- front/js/app/Models/Abstract.js
+++ front/js/app/Models/Abstract.js
@@ -2,14 +2,8 @@ Models.Abstract = Class.extend({
 	
 	_data: null,
 	
-	/**
-	 * @type Array 
-	 */
-	_on_change_subscribers: null,
-	
 	initialize: function(data){
 		this._data = data;
-		this._on_change_subscribers = [];
 	},
 	
 	get: function(key){
@@ -17,42 +11,18 @@ Models.Abstract = Class.extend({
 	},
 	
 	set: function(key, value){
-		
-		this._set(key, value);
-		
-		var changed_data = {};
-		changed_data[key] = value;
-		
-		this._runOnChangeSubscribers(changed_data);		
-		
+		this._data[key] = value;
 		return this;
 	},
 	
 	update: function(data)
 	{
 		for(var i in data){
-			this._set(i, data[i]);
+			this.set(i, data[i]);
 		}
-		
-		this._runOnChangeSubscribers(data);
 	},
 	
 	getAll: function(){
 		return this._data;
-	},
-	
-	onChange: function(func){
-		this._on_change_subscribers.push(func);
-	},
-	
-	_set: function(key, value){
-		this._data[key] = value;
-	},
-	
-	_runOnChangeSubscribers: function(changed_data){
-		
-		for (var i in this._on_change_subscribers){
-			this._on_change_subscribers[i](changed_data, this);
-		}
 	}
 });
--- front/js/app/bin/0273b494d66af726729c3817b9e194e3.js
+++ front/js/app/bin/0273b494d66af726729c3817b9e194e3.js
@@ -23,14 +23,8 @@ Models.Abstract = Class.extend({
 	
 	_data: null,
 	
-	/**
-	 * @type Array 
-	 */
-	_on_change_subscribers: null,
-	
 	initialize: function(data){
 		this._data = data;
-		this._on_change_subscribers = [];
 	},
 	
 	get: function(key){
@@ -38,43 +32,19 @@ Models.Abstract = Class.extend({
 	},
 	
 	set: function(key, value){
-		
-		this._set(key, value);
-		
-		var changed_data = {};
-		changed_data[key] = value;
-		
-		this._runOnChangeSubscribers(changed_data);		
-		
+		this._data[key] = value;
 		return this;
 	},
 	
 	update: function(data)
 	{
 		for(var i in data){
-			this._set(i, data[i]);
+			this.set(i, data[i]);
 		}
-		
-		this._runOnChangeSubscribers(data);
 	},
 	
 	getAll: function(){
 		return this._data;
-	},
-	
-	onChange: function(func){
-		this._on_change_subscribers.push(func);
-	},
-	
-	_set: function(key, value){
-		this._data[key] = value;
-	},
-	
-	_runOnChangeSubscribers: function(changed_data){
-		
-		for (var i in this._on_change_subscribers){
-			this._on_change_subscribers[i](changed_data, this);
-		}
 	}
 });
 /**
