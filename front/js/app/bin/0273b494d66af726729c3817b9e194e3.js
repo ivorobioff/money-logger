b9535543ba0bae6ec20cf563a94aaa6cabe6b01e
ivorobioff <i_am_vib@yahoo.com>
2013-08-02 00:29:52 +0300

    sdf

--- front/js/app/bin/0273b494d66af726729c3817b9e194e3.js
+++ front/js/app/bin/0273b494d66af726729c3817b9e194e3.js
@@ -20,10 +20,17 @@ Views.GroupsCollection = Class.extend({
 
 create_singleton(Views.GroupsCollection);
 Models.Abstract = Class.extend({
+	
 	_data: null,
+	
+	/**
+	 * @type Array 
+	 */
+	_on_change_subscribers: null,
 	
 	initialize: function(data){
 		this._data = data;
+		this._on_change_subscribers = [];
 	},
 	
 	get: function(key){
@@ -31,19 +38,43 @@ Models.Abstract = Class.extend({
 	},
 	
 	set: function(key, value){
-		this._data[key] = value;
+		
+		this._set(key, value);
+		
+		var changed_data = {};
+		changed_data[key] = value;
+		
+		this._runOnChangeSubscribers(changed_data);		
+		
 		return this;
 	},
 	
 	update: function(data)
 	{
 		for(var i in data){
-			this._data[i] = data[i];
+			this._set(i, data[i]);
 		}
+		
+		this._runOnChangeSubscribers(data);
 	},
 	
 	getAll: function(){
 		return this._data;
+	},
+	
+	onChange: function(func){
+		this._on_change_subscribers.push(func);
+	},
+	
+	_set: function(key, value){
+		this._data[key] = value;
+	},
+	
+	_runOnChangeSubscribers: function(changed_data){
+		
+		for (var i in this._on_change_subscribers){
+			this._on_change_subscribers[i](changed_data, this);
+		}
 	}
 });
 /**
@@ -82,8 +113,7 @@ Collections.Abstract = Class.extend({
 	
 	remove: function(id){
 		delete this._models[id];
-	}
-	
+	}	
 });
 /**
  * @load Collections.Abstract
@@ -354,7 +384,7 @@ Views.ConfirmDialog = Views.AbstractDialog.extend({
 	_getContentLabels: function(){
 		return {
 			text: this._options.text
-		}
+		};
 	},
 	
 	getContext: function(){
