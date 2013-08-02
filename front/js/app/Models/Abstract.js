743a6df2b9c1374e9cd738c906702983321854c7
ivorobioff <i_am_vib@yahoo.com>
2013-08-02 08:08:07 +0300

    new model

--- front/js/app/Models/Abstract.js
+++ front/js/app/Models/Abstract.js
@@ -1,9 +1,14 @@
+/**
+ * @load Libs.Event
+ */
 Models.Abstract = Class.extend({
 	
 	_data: null,
+	_event: null,
 	
 	initialize: function(data){
 		this._data = data;
+		this._event = new Libs.Event();
 	},
 	
 	get: function(key){
@@ -11,18 +16,36 @@ Models.Abstract = Class.extend({
 	},
 	
 	set: function(key, value){
-		this._data[key] = value;
+		this._set(key, value);		
+		this._event.trigger("set:" + key, [value, this]);
 		return this;
 	},
 	
 	update: function(data)
 	{
 		for(var i in data){
-			this.set(i, data[i]);
+			this._set(i, data[i]);
 		}
+		
+		this._event.trigger("update", [this]);
+		return this;
 	},
 	
 	getAll: function(){
 		return this._data;
+	},
+	
+	onUpdate: function(callback){
+		this._event.add("update", callback);
+		return this;
+	},
+	
+	onSet: function(key, callback){
+		this._event.add("set:" + key, callback);
+		return this;
+	},
+	
+	_set: function(key, value){
+		this._data[key] = value;
 	}
 });
--- front/js/app/bin/0273b494d66af726729c3817b9e194e3.js
+++ front/js/app/bin/0273b494d66af726729c3817b9e194e3.js
@@ -1,35 +1,3 @@
-Models.Abstract = Class.extend({
-	
-	_data: null,
-	
-	initialize: function(data){
-		this._data = data;
-	},
-	
-	get: function(key){
-		return this._data[key];
-	},
-	
-	set: function(key, value){
-		this._data[key] = value;
-		return this;
-	},
-	
-	update: function(data)
-	{
-		for(var i in data){
-			this.set(i, data[i]);
-		}
-	},
-	
-	getAll: function(){
-		return this._data;
-	}
-});
-/**
- * @load Models.Abstract
- */
-Models.Group = Models.Abstract.extend({});
 Libs.Event = Class.extend({
 	_events: null,
 	
@@ -57,6 +25,61 @@ Libs.Event = Class.extend({
 		}
 	}
 });
+/**
+ * @load Libs.Event
+ */
+Models.Abstract = Class.extend({
+	
+	_data: null,
+	_event: null,
+	
+	initialize: function(data){
+		this._data = data;
+		this._event = new Libs.Event();
+	},
+	
+	get: function(key){
+		return this._data[key];
+	},
+	
+	set: function(key, value){
+		this._set(key, value);		
+		this._event.trigger("set:" + key, [value, this]);
+		return this;
+	},
+	
+	update: function(data)
+	{
+		for(var i in data){
+			this._set(i, data[i]);
+		}
+		
+		this._event.trigger("update", [this]);
+		return this;
+	},
+	
+	getAll: function(){
+		return this._data;
+	},
+	
+	onUpdate: function(callback){
+		this._event.add("update", callback);
+		return this;
+	},
+	
+	onSet: function(key, callback){
+		this._event.add("set:" + key, callback);
+		return this;
+	},
+	
+	_set: function(key, value){
+		this._data[key] = value;
+	}
+});
+/**
+ * @load Models.Abstract
+ */
+Models.Group = Models.Abstract.extend({});
 /**
  * @load Libs.Event
  */
